5b6cbb44f8318f1519758c5ea145ab1313d6afd8
Leillen <150943989+Leillen59@users.noreply.github.com>
2024-12-16 11:26:42 +0100

    header_creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes} from '@angular/router';
 
-export const routes: Routes = [];
+//Importation des différentes pages
+import { HomeComponent } from './Pages/home/home.component.js';
+import { AboutComponent } from './Pages/about/about.component.js';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'Accueil', pathMatch: 'full' },
+    { path: 'Accueil', component: HomeComponent },
+    { path: 'About', component: AboutComponent },
+];
