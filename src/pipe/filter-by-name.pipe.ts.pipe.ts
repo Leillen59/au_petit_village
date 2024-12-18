120f179058f347c77413c5797c401277f22b769b
Leillen <150943989+Leillen59@users.noreply.github.com>
2024-12-18 11:35:39 +0100

    homepage and product creation

---
+++ src/pipe/filter-by-name.pipe.ts.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from '../app/models/product.js';
+@Pipe({
+  name: 'productFilter',
+  standalone: true,
+  pure: false
+})
+export class ProductFilterPipe implements PipeTransform {
+  transform(products: Product[], searchQuery: string): Product[] {
+    if (!products) return [];
+    if (!searchQuery) return products;
+
+    searchQuery = searchQuery.toLowerCase().trim();
+
+    return products.filter(product =>
+      product.name.toLowerCase().includes(searchQuery)
+    );
+  }
+}
